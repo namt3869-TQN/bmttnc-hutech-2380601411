26ca60c1a57c037b32e739bcb584f9fcc9e5621a
Tran Quoc Nam <namt3869@gmail.com>
2026-05-28 23:31:47 +0700

    [fix] merge local lab-01 files with remote

---
+++ README.md
@@ -0,0 +1,2 @@
+# bmttnc-hutech-2380601411
+Trần Quốc Nam _ 2380601411
---
+++ lab-01/ex02/ex02_02.py
@@ -0,0 +1,6 @@
+# Nhập bán kính từ người dùng
+ban_khinh = float(input("Nhập bán kính của hình tròn: "))
+# Tính diện tích của hình tròn
+dien_tich = 3.14 * (ban_khinh ** 2)
+# In diện tích của hình tròn ra màn hình
+print("Diện tích của hình tròn là:", dien_tich)
